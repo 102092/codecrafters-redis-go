82668f5c583021850fbf1769cc76072686751de1
102092 <quaint3@gmail.com>
2025-08-24 21:31:43 +0900

    Add handle connection for concurrent request

--- app/main.go
+++ app/main.go
@@ -14,22 +14,31 @@ var _ = os.Exit
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests
-	
+
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleConnection(conn)
 	}
-	
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
 	scanner := bufio.NewScanner(conn)
 	for scanner.Scan() {
 		line := scanner.Text()
-		if strings.EqualFold(line, "PING") {
+		if strings.Contains(strings.ToUpper(line), "PING") || strings.EqualFold(line, "PING") {
 			_, _ = conn.Write([]byte("+PONG\r\n"))
 		}
 	}
